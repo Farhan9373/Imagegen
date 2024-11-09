57d0677a8fc9628320a72d50b4d2b2f85eee8034
Farhan Ahmad <farhanahmadptn@gmail.com>
2024-11-09 22:09:26 +0530

    final changes done

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -39,9 +39,12 @@ const CreatePost = () => {
         });
 
         const data = await response.json();
+        console.log(data); // Check what data contains
+
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
       } catch (err) {
         alert(err);
+        console.log("error",err);
       } finally {
         setGeneratingImg(false);
       }
@@ -56,7 +59,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
+  
 });
 
 const openai = new OpenAIApi(configuration);
@@ -30,9 +31,12 @@ router.route('/').post(async (req, res) => {
     const image = aiResponse.data.data[0].b64_json;
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.error(error);
-    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    console.error("OpenAI API Error:", error);
+
+    const errorMessage = error.response?.data?.error?.message || 'Something went wrong';
+    res.status(500).json({ error: errorMessage });
   }
 });
+
 
 export default router;
